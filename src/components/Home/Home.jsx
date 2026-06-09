25f7f838d2057991ca1c8d6805029e9b5e303c01
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-06-09 17:40:44 +0300

    fix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -42,12 +42,12 @@ export default function Home() {
                 </h3>
                 <p className="truncate capitalize">{product.description}</p>
 
-                <div className="flex items-center justify-between">
-                  <div className="flex items-center space-x-3 ">
+                <div className="flex items-center ">
+                  <div className="flex items-center space-x-3 text-sm">
                     <h5 className="font-medium">{product.price} EGP</h5>
                     <h5 className="line-through text-gray-500">750 EGP</h5>
                   </div>
-                  <div className="flex mt-1 items-center text-xs">
+                  <div className="flex mt-1 items-center text-xs ml-auto">
                     <h6 className="">4.9</h6>
                     <i className="fa-solid fa-star text-[#ECB018]"></i>
                   </div>
