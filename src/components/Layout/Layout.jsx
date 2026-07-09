c5f9ff3263ccad96af71e9b7319722fc7441c646
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-07-09 12:11:12 +0300

    fdas

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -34,7 +34,7 @@ export default function Layout() {
           key={pathname}
           initial={{ opacity: 0, y: 15 }}
           animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -15 }}
+          // exit={{ opacity: 0, y: -15 }}
           transition={{ duration: 0.3, ease: "easeInOut" }}
         >
           <Navbar />
