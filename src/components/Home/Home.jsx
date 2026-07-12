80876b43dff5deb6617c5f64f31e30f6735cb36f
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-07-12 15:44:49 +0300

    fdsa

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -52,51 +52,6 @@ export default function Home() {
         <img src="/bg.webp" className="w-full" alt="" />
       </div>
       <div className="padding grid md:grid-cols-2 grid-cols-1 gap-3">
-        {/* {data?.map((product, i) => (
-          <motion.div
-            key={i}
-            initial={{ opacity: 0, scale: 0.7 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            viewport={{ once: true, margin: "-50px" }}
-            transition={{ duration: 0.35 }}
-            className="mb-6 relative overflow-hidden"
-          >
-            {!product.inStock && (
-              <span className="absolute bg-red-500 z-50 top-5 -right-11 lg:top-7 lg:-right-8 px-2 py-0.5 w-36 text-center rotate-45 text-white text-sm">
-                {" "}
-                Sold out
-              </span>
-            )}
-            <NavLink to={`/details/${product._id}`}>
-              <div className=" overflow-hidden mb-4 rounded-md aspect-2/3 ">
-                <img
-                  src={product.colors[0].image}
-                  alt=""
-                  className="image w-full h-full object-cover"
-                />
-              </div>
-              <div className="text-sm">
-                <h3 className="capitalize font-medium text-burgundy truncate ">
-                  {product.name}
-                </h3>
-                <p className="truncate capitalize text-xs mb-2">
-                  {product.description}
-                </p>
-
-                <div className="flex items-center ">
-                  <div className="flex items-center space-x-3 text-sm">
-                    <h5 className="font-medium">{product.price} LE</h5>
-                  </div>
-                  <div className="flex mt-1 items-center text-xs ml-auto">
-                    <h6 className="">4.9</h6>
-                    <i className="fa-solid fa-star text-[#ECB018]"></i>
-                  </div>
-                </div>
-              </div>
-            </NavLink>
-          </motion.div>
-        ))} */}
-
         {Images.map((e, i) => (
           <div key={i} className="aspect-3/4 overflow-hidden relative">
             {/* overlay */}
@@ -104,7 +59,7 @@ export default function Home() {
               <div className="absolute left-7 bottom-7  md:left-12 md:bottom-12 z-20">
                 <h5 className="text-white capitalize  text-xl">{e.name}</h5>
                 <NavLink
-                  className={`bg-white  category text-black font-light px-5 py-1.5 mt-3 md:mt-5 block capitalize border border-transparent `}
+                  className="bg-white category text-black font-light px-5 py-1.5 mt-3 md:mt-5 block capitalize border border-transparent"
                   to={`category/${e.name}`}
                 >
                   {`step in to ${e.name}`}
